4f110b5c1a4793a477e3b903878fb29d79ffb727
cyz <chenyongzhijjk@163.com>
2019-07-01 22:23:20 +0800

    js

--- javascript/js/a.js
+++ javascript/js/a.js
@@ -2,3 +2,16 @@ var a = 1;
 var b = 2;
 console.log(a + b);
 
+var arr = ["ab", 2, false, undefined];
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
+// }
+
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+for (element in arr) {
+    console.log(element);
+}
